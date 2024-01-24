a1a1b4974318f29c09805c0a8701b2e57dde286e
Melvin Aguilar <90595158+MelvinAguilar@users.noreply.github.com>
2024-01-24 19:36:20 +0000

    :sparkles: Add: document, query and aggregation middlewares

--- backend/src/models/tour.model.ts
+++ backend/src/models/tour.model.ts
@@ -1,7 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose, { Query } from 'mongoose';
+const slugify = require('slugify');
+
+interface ITour {
+  name: string;
+  slug: string;
+  duration: number;
+  maxGroupSize: number;
+  difficulty: string;
+  ratingsAverage: number;
+  ratingsQuantity: number;
+  price: number;
+  priceDiscount: number;
+  summary: string;
+  description: string;
+  imageCover: string;
+  images: string[];
+  createdAt: Date;
+  startDates: Date[];
+  secretTour: boolean;
+}
 
 // Define the tour schema
-const tourSchema = new mongoose.Schema(
+const tourSchema = new mongoose.Schema<ITour>(
   {
     // Specify the name for the tour
     name: {
@@ -53,6 +73,12 @@ const tourSchema = new mongoose.Schema(
     // Discounted price for the tour
     priceDiscount: {
       type: Number,
+      validate: {
+        validator: function(this: any, val: number) {
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price'
+      }
     },
     // Brief summary of the tour
     summary: {
@@ -97,6 +123,33 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+// This middleware is used to create a slug for the tour when it is saved 
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// This middleware is used to hide secret tours from the query results (find
+tourSchema.pre(/^find/, function(next) {
+  (this as Query<any, any>).find({ secretTour: { $ne: true } });
+
+  (this as any).start = Date.now();
+  next();
+});
+
+// This middleware is used to calculate the time taken for the query to execute
+tourSchema.post(/^find/, function(_docs, next) {
+  console.log(`Query took ${Date.now() - (this as any).start} milliseconds!`);
+  next();
+});
+
+// This middleware is used to populate the tour with the guides
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
+  next();
+});
 
 // Create the Tour model
 const Tour = mongoose.model('Tour', tourSchema);
