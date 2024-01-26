7b5be497b7b81e6c983bbf84bf0c95a3d10b2222
Melvin Aguilar <90595158+MelvinAguilar@users.noreply.github.com>
2024-01-26 23:49:15 +0000

    ✨ Add:HTTP secure headers, rate limit, data sanitization, and parameter pollution middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,54 @@ import express from 'express';
 const morgan = require('morgan');
 const indexRouter = require('./routes/index.router');
 const globalErrorHandler = require('./controllers/error.controller');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const app = express();
 
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Rate limiter to control the number of requests from a single IP address
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
+// Body parser to read data from the request body into req.body (data size limited to 10kb)
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization to protect against NoSQL query injection attacks
+app.use(mongoSanitize());
+
+// Data sanitization to protect against cross-site scripting (XSS) attacks
+app.use(xss());
+
+// HTTP parameter pollution (HPP) middleware to prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'price',
+      'duration',
+      'difficulty',
+      'maxGroupSize',
+      'ratingsAverage',
+      'ratingsQuantity',
+    ],
+  }),
+);
+
+// Routes
 app.use('/api/v1', indexRouter);
 
 app.use(globalErrorHandler);
